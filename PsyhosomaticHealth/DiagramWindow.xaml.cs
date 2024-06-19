283d61f2a3fb3212aeffd69c53bfb98c35392444
eviolspirid777 <eviolspirid@gmail.com>
2024-06-19 20:58:37 +0300

    изменил график

--- PsyhosomaticHealth/DiagramWindow.xaml.cs
+++ PsyhosomaticHealth/DiagramWindow.xaml.cs
@@ -25,18 +25,22 @@ namespace PsyhosomaticHealth
 		public DiagramWindow()
 		{
 			InitializeComponent();
-			PlotModel plotModel = new PlotModel { Title = "График зависимости", Background = OxyColor.FromRgb(240,240,240) };
+			var plotModel = new PlotModel { Title = "График зависимости", Background = OxyColor.FromRgb(240, 240, 240) };
 
-			// Создаем график функции y = sin(x)
+			// Меняем названием осей X и Y
+			plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Bottom, Title = "Время" });
+			plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Left, Title = "Пульс" });
+
+			var random = new Random();
+
+			// Создаем график с точками, распределенными хаотичным образом с дисперсией 0.2
 			var series = new LineSeries();
 			for (double x = 0; x < 10; x += 0.1)
 			{
-				series.Points.Add(new DataPoint(x, Math.Sin(x)));
+				double y = 80 + random.NextDouble() * 30; // Значение y в диапазоне от 80 до 110
+				series.Points.Add(new DataPoint(x, y));
 			}
 			plotModel.Series.Add(series);
-
-			plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Bottom, Title = "Время" });
-			plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Left, Title = "Пульс" });
 
 			diagram.Model = plotModel;
 		}
